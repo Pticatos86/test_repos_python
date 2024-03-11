63add37a7add5c1afe0b8778eaa869289e3c57ab
Pticatos86 <pticatos86@gmail.com>
2024-03-11 21:24:58 +0200

    modified gitignore

---
+++ Calculator_Lesson_3.py
@@ -0,0 +1,19 @@
+action = input("Choose your action (+, -, *, /): ")
+num_1 = float(input("Number_1 = "))
+num_2 = float(input("Number_2 = "))
+
+if action == '+':
+    print(num_1 + num_2)
+elif action == '-':
+    print(num_1 - num_2)
+elif action == '*':
+    print(num_1 * num_2)
+elif action == '/':
+    if num_2 != 0:
+        print(num_1 / num_2)
+    else:
+        print("Division by zero")
+else:
+    print("Enter valid operation and try again")
+
+
