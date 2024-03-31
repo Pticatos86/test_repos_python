711d253fac807b05996dbd36fa24768318a84ef5
Pticatos86 <pticatos86@gmail.com>
2024-03-31 21:40:54 +0300

    fixed all tasks, understood my mistakes

--- dictionary/homework_dictionary.py
+++ dictionary/homework_dictionary.py
@@ -6,50 +6,31 @@ roles_dict = {
     'manager': ['Piter', 'Max', 'Sophia'],
     'developer': ['Natali', 'Nikita', 'Diana']
 }
-list_system_names = []
-system_role = ''
 
-for roles, list_names in roles_dict.items():
-    for names in list_names:
-        list_system_names.append(names)
-        if check_name in names:
-            system_role = roles
-        else:
-            pass
-"""I think for correcting end current loops should be block else,
-but I don`t know what to write in this block else.
-Is it correct if i write else: pass?"""
-
-if check_name in list_system_names:
-    print(f"Hello {system_role}")
-else:
-    print("Hello guest")
+for role, list_names in roles_dict.items():
+    if check_name in list_names:
+        print(f"Hello {role}")
+        break
+    else:
+        print("Hello guest!")
 
 """Solution task 2"""
 params = {'v': 'some_id', 'list': 'some_list', 'index': '6', 't': 'Os'}
 initial_str = 'https://www.youtube.com/watch?'
 
-params_list = []
 for key, value in params.items():
-    params_list.append(key + '=' + value)
+    initial_str += f"{key}={value}&"
 
-params_string = '&'.join(params_list)
-print(f"Result link is {initial_str + params_string}")
+result = initial_str.strip('&')
+
+print(f"Adding a string to the dictionary: {result}")
 
 """Solution task 3"""
 result_link = 'https://www.youtube.com/watch?v=some_id&list=some_list&index=6&t=Os'
-result_dict = {}
+dict_count = {}
 
-key_one = 'w'
-value_one = result_link.count(key_one)
-result_dict[key_one] = value_one
+for element in result_link:
+    if element in result_link:
+        dict_count[element] = result_link.count(element)
 
-key_two = 'a'
-value_two = result_link.count(key_two)
-result_dict[key_two] = value_two
-
-key_three = 't'
-value_three = result_link.count(key_three)
-result_dict[key_three] = value_three
-
-print(f"Result dictionary from link {result_link} is {result_dict}")
+print(f"How many times an element is repeated in a line {dict_count}")
