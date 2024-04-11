d61c33f031aac1393fae0e30f41fdf41a89d8987
Pticatos86 <pticatos86@gmail.com>
2024-04-11 14:35:22 +0300

    added test file

---
+++ Modules_and_packages/implementation_temp_convert.py
@@ -0,0 +1,5 @@
+from Modules_and_packages.mathematics_function import convert_temp
+
+if __name__ == '__main__':
+    conversion_result = convert_temp(100, 'F')
+    print(f"Your conversion result is {conversion_result}")
---
+++ test.py
@@ -0,0 +1,3 @@
+from Modules_and_packages.mathematics_function import convert_temp
+
+print(convert_temp(200, 'F'))
